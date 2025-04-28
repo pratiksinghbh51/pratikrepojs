cd935b35c076a68e422b862ca442295b39ec8d9e
pratiksinghbh51 <Pratiksinghbh51@gmail.com>
2025-04-28 09:25:40 +0000

    newone

--- JSnew/test.js
+++ JSnew/test.js
@@ -1,1 +1,11 @@
- console.log("Pratik Singh Bhardwaj")
+let firstName = "Pratik"
+let office = "Bhubneshwar"
+let call = "76262626"
+let number = 12
+
+let convert = Number(office)
+let convert01 = Number(call)
+console.log(convert)
+console.log(typeof convert01)
+
+console.table([firstName,office,call,number])
